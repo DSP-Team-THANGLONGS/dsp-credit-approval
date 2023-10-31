8ddb808d03b483560e349a240118f92540831d39
Shashank Vaidya <129398861+shashank-vaidya@users.noreply.github.com>
2023-10-31 16:19:10 +0100

    Feature/streamlit (#6)
    * Streamlit v1
    
    * Streamlit v2
    
    ---------
    
    Co-authored-by: MasonVu <56074169+VMD121199@users.noreply.github.com>

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,9 +1,14 @@
-# BEGIN: 3j4k5l6m7n8o
 import streamlit as st
-
+import json
+import datetime
+ 
+ 
 def main():
     st.title("Credit Card Approval Form")
-    st.write("Please fill out the following form to check your eligibility for a credit card.")
+    st.write(
+        "Please fill out the following form to check your eligibility for a credit card."
+    )
+ 
 
     # Question 1
     st.write("Do you own a car?")
@@ -13,46 +18,149 @@ def main():
     # Question 2
     st.write("Do you own any piece of real estate?")
     real_estate_options = ["Yes", "No"]
-    real_estate = st.radio("Select an option", real_estate_options, key="radio2")
+    real_estate = st.radio(
+        "Select an option", real_estate_options, key="radio2"
+    )
+ 
+    # Question Birthday
+    st.write("What is your birthday?")
+    birthday = st.date_input("Calendar", datetime.date(1999, 11, 12))
+ 
 
     # Question 3
     st.write(" Do you have children?")
     children_options = ["Yes", "No"]
     children = st.radio("Select an option", children_options, key="radio3")
-    
+    children_count = 0
     if children == "Yes":
         st.write("How many children do you have?")
         children_count_options = list(range(11))
-        children_count = st.selectbox("Select an option", children_count_options, key="select1")
-
+        children_count = st.selectbox(
+            "Select an option", children_count_options, key="select1"
+        )
+ 
     # Question 5
     st.write("What is your yearly income?")
-    income = st.number_input("Enter your yearly income", min_value=0.0, format="%f", key="number1")
-
+    income = st.number_input(
+        "Enter your yearly income", min_value=0.0, format="%f", key="number1"
+    )
+ 
     # Question 6
     st.write("What is your source of income?")
-    occupation_options = ["Working", "Commercial associate", "Pensioner", "State servant", "Student"]
-    occupation = st.selectbox("Select an option", occupation_options, key="select2")
-
+    income_type_options = [
+        "Working",
+        "Commercial associate",
+        "Pensioner",
+        "State servant",
+        "Student",
+    ]
+    income_type = st.selectbox(
+        "Select an option", income_type_options, key="select2"
+    )
+ 
+    st.write("Are you still working?")
+    working_options = ["Yes", "No"]
+    isWorking = st.radio("Select an option", working_options, key="radio4")
+    if isWorking == "Yes":
+        st.write("The day you start working?")
+        workday = st.date_input("Calendar", datetime.date(2020, 10, 20))
+    else:
+        st.write("The day you stop working?")
+        workday = st.date_input("Calendar", datetime.date(2020, 10, 20))
+ 
+    st.write("What is your working domain?")
+    occupation_options = [
+        "Other",
+        "Security staff",
+        "Sales staff",
+        "Accountants",
+        "Laborers",
+        "Core staff",
+        "Managers",
+        "Drivers",
+        "Cleaning staff",
+        "Private service staff",
+        "High skill tech staff",
+        "Low-skill Laborers",
+        "Cooking staff",
+        "Medicine staff",
+        "Secretaries",
+        "HR staff",
+        "Waiters/barmen staff",
+        "Realty agents",
+        "IT staff",
+    ]
+    occupation = st.selectbox(
+        "Select an option", occupation_options, key="select3"
+    )
+ 
     # Question 7
     st.write("What is your education level?")
-    education_options = ["Higher education", "Secondary / secondary special", "Incomplete higher", "Lower secondary", "Academic degree"]
-    education = st.selectbox("Select an option", education_options, key="select3")
-
+    education_options = [
+        "Higher education",
+        "Secondary / secondary special",
+        "Incomplete higher",
+        "Lower secondary",
+        "Academic degree",
+    ]
+    education = st.selectbox(
+        "Select an option", education_options, key="select4"
+    )
+ 
     # Question 8
     st.write("What is your marital status?")
-    marital_status_options = ["Civil marriage", "Married", "Single / not married", "Separated", "Widow"]
-    marital_status = st.selectbox("Select an option", marital_status_options, key="select4")
-
+    marital_status_options = [
+        "Civil marriage",
+        "Married",
+        "Single / not married",
+        "Separated",
+        "Widow",
+    ]
+    marital_status = st.selectbox(
+        "Select an option", marital_status_options, key="select5"
+    )
+ 
     # Question 9
     st.write("What is your housing situation?")
-    housing_options = ["Rented apartment", "House / apartment", "Municipal apartment", "With parents", "Co-op apartment", "Office apartment"]
-    housing = st.selectbox("Select an option", housing_options, key="select5")
-
+    housing_options = [
+        "Rented apartment",
+        "House / apartment",
+        "Municipal apartment",
+        "With parents",
+        "Co-op apartment",
+        "Office apartment",
+    ]
+    housing = st.selectbox("Select an option", housing_options, key="select6")
+ 
     # Question 10
     st.write("How many members are there in your family?")
     family_members_options = list(range(1, 21))
-    family_members = st.selectbox("Select an option", family_members_options, key="select6")
-
+    family_members = st.selectbox(
+        "Select an option", family_members_options, key="select7"
+    )
+ 
+    if st.button("Submit prediction", type="primary"):
+        birthday_calc = birthday - datetime.date.today()
+        employed_calc = (
+            workday - datetime.date.today()
+            if isWorking == "YES"
+            else datetime.date.today() - workday
+        )
+        data = {
+            "FLAG_OWN_CAR": car[0],
+            "FLAG_OWN_REALTY": real_estate[0],
+            "AMT_INCOME_TOTAL": income,
+            "NAME_INCOME_TYPE": income_type,
+            "NAME_EDUCATION_TYPE": education,
+            "NAME_FAMILY_STATUS": marital_status,
+            "NAME_HOUSING_TYPE": housing,
+            "DAYS_BIRTH": birthday_calc.days,
+            "DAYS_EMPLOYED": employed_calc.days,
+            "OCCUPATION_TYPE": occupation,
+            "CNT_FAM_MEMBERS": family_members,
+        }
+        print(data)
+ 
+ 
 if __name__ == "__main__":
     main()
