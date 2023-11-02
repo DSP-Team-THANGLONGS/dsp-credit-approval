ca30da057ca277973b407a2f0c03742fbf7c7a8c
MasonVu <56074169+VMD121199@users.noreply.github.com>
2023-11-02 16:23:17 +0100

    fix data type of birthday, employed day and still working (#13)

--- api/database/models.py
+++ api/database/models.py
@@ -1,5 +1,5 @@
 from .database import Base
-from sqlalchemy import Column, Date, Integer, String, Float
+from sqlalchemy import Column, Date, Integer, String, Float, Boolean
 
 
 class Records(Base):
@@ -12,8 +12,9 @@ class Records(Base):
     education = Column(String, index=True)
     family_status = Column(String, index=True)
     housing_type = Column(String, index=True)
-    birthday = Column(Integer)
-    employed_day = Column(Integer)
+    birthday = Column(Date)
+    employed_day = Column(Date)
+    still_working = Column(Boolean)
     occupation = Column(String, index=True)
     fam_members = Column(Integer)
     result = Column(Integer)
