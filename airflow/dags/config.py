f58687105b0b1a2da0da06b4243421db0d81ac7c
VMD121199 <minhducvu1211@gmail.com>
2024-01-11 15:15:33 +0100

    update new dags

--- airflow/dags/config.py
+++ airflow/dags/config.py
@@ -1,2 +1,2 @@
-URL_PREDICT = "http://172.21.112.1:8000/predict"
+URL_PREDICT = "http://172.28.16.1:8000/predict"
 URL_GET_PREDICTIONS = "http://192.168.1.54:8000/get-predictions"
--- airflow/dags/predict_job.py
+++ airflow/dags/predict_job.py
@@ -16,6 +16,7 @@ import requests
     tags=["dsp", "data_prediction"],
     schedule=timedelta(minutes=4),
     start_date=days_ago(n=0, hour=0),
+    catchup=False,
 )
 def predict_data():
     @task()
@@ -27,51 +28,59 @@ def predict_data():
 
         for file in os.listdir(folder_path):
             file_path = os.path.join(folder_path, file)
-            if os.path.isfile(file_path) and file.endswith(".csv"):
-                if file.startswith("predicted_"):
-                    continue
-                df = pd.read_csv(file_path)
-                df_list.append(df)
-                os.rename(
-                    file_path,
-                    folder_path + "/predicted_" + file,
-                )
+            if file.startswith("predicted_"):
+                continue
+            df = pd.read_csv(file_path)
+            df_list.append(df)
+            os.rename(file_path, folder_path + "/predicted_" + file)
+
         if len(df_list) > 0:
             merged_df = pd.concat(df_list, ignore_index=True)
-            return merged_df.to_json(orient="records")
+            data_to_ingest_json = merged_df.to_json(orient="records")
+            return data_to_ingest_json
         else:
-            return False
+            return None
 
     @task
     def make_prediction(data_to_ingest_json):
-        df = pd.read_json(data_to_ingest_json, orient="records")
-        df["PLATFORM"] = "JOB"
-        data = df[
-            [
-                "FLAG_OWN_CAR",
-                "FLAG_OWN_REALTY",
-                "AMT_INCOME_TOTAL",
-                "NAME_INCOME_TYPE",
-                "NAME_EDUCATION_TYPE",
-                "NAME_FAMILY_STATUS",
-                "NAME_HOUSING_TYPE",
-                "DAYS_BIRTH",
-                "DAYS_EMPLOYED",
-                "OCCUPATION_TYPE",
-                "CNT_FAM_MEMBERS",
-                "PLATFORM",
-            ]
-        ].to_dict(orient="records")
-        response = requests.post(config.URL_PREDICT, data=json.dumps(data))
-        prediction_str = response.json()
-        prediction = ast.literal_eval(prediction_str)
+        if data_to_ingest_json is not None:
+            df = pd.read_json(data_to_ingest_json, orient="records")
+            df["PLATFORM"] = "JOB"
 
-        df["DATE_PREDICTED"] = datetime.now().strftime("%Y-%m-%d")
-        df["APPROVED"] = prediction
+            data = df[
+                [
+                    "FLAG_OWN_CAR",
+                    "FLAG_OWN_REALTY",
+                    "AMT_INCOME_TOTAL",
+                    "NAME_INCOME_TYPE",
+                    "NAME_EDUCATION_TYPE",
+                    "NAME_FAMILY_STATUS",
+                    "NAME_HOUSING_TYPE",
+                    "DAYS_BIRTH",
+                    "DAYS_EMPLOYED",
+                    "OCCUPATION_TYPE",
+                    "CNT_FAM_MEMBERS",
+                    "PLATFORM",
+                ]
+            ].to_dict(orient="records")
+
+            response = requests.post(config.URL_PREDICT, data=json.dumps(data))
+            prediction_str = response.json()
+            prediction = ast.literal_eval(prediction_str)
+
+            df["DATE_PREDICTED"] = datetime.now().strftime("%Y-%m-%d")
+            df["APPROVED"] = prediction
+
+            # Save the predicted data to a new file
+            df.to_csv(
+                os.path.join(
+                    "/home/mdv/dsp-credit-approval/airflow/data/predicted_data.csv"
+                ),
+                index=False,
+            )
 
     data_json = check_for_new_data()
-    if data_json:
-        make_prediction(data_json)
+    make_prediction(data_json)
 
 
 ingest_data_dag = predict_data()
